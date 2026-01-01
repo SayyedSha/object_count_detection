7cd9e54d90a551c6505eea48e312068752785b54
Shahanawaz <shahanawazsayyed0@gmail.com>
2026-01-01 15:28:45 +0530

    - Gave CORS access to all - Changed response

--- app/main.py
+++ app/main.py
@@ -2,11 +2,25 @@ import os
 import uuid
 import shutil
 from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.detector import detect_and_count_objects
 from app.cloudinary_uploader import upload_video
 
 app = FastAPI()
+
+# -----------------------------
+# CORS CONFIG
+# -----------------------------
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 UPLOAD_DIR = "temp/uploads"
 OUTPUT_DIR = "temp/outputs"
@@ -17,39 +31,32 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 @app.post("/process-video")
 def process_video(file: UploadFile = File(...)):
-    # -----------------------------
-    # Validate file type
-    # -----------------------------
     if not file.content_type.startswith("video/"):
         raise HTTPException(status_code=400, detail="Invalid video file")
 
     video_id = str(uuid.uuid4())
-
     input_path = os.path.join(UPLOAD_DIR, f"{video_id}_{file.filename}")
     output_path = os.path.join(OUTPUT_DIR, f"{video_id}_output.mp4")
 
-    # -----------------------------
-    # Save uploaded video (SAFE)
-    # -----------------------------
     with open(input_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # -----------------------------
-    # Run YOLO detection ONCE
-    # -----------------------------
     counts = detect_and_count_objects(
         video_path=input_path,
         output_video_path=output_path,
-        model_path="yolo11l.pt"   # choose ONE model
+        model_path="yolo11l.pt"
     )
 
-    # -----------------------------
-    # Upload to Cloudinary
-    # -----------------------------
+    counts_array = [
+        {"object": cls, "count": count}
+        for cls, count in counts.items()
+    ]
+
     video_url = upload_video(output_path)
 
     return {
         "status": "success",
         "video_url": video_url,
-        "counts": counts
+        "counts": counts_array
     }
+
